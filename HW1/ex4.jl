9dff744ebabdc79c778dc3ee9427334984b084b7
ERATO <erato@Ivars-MBP.lan>
2020-11-08 23:31:33 +0100

    sunday

--- HW1/ex4.jl
+++ HW1/ex4.jl
@@ -1,54 +1,49 @@
-include("arnoldi.jl")
-using LinearAlgebra, Arpack, MatrixDepot, Random
+using LinearAlgebra, Arpack, MatrixDepot, Random, Plots
 
-nn=10;
-m = 15;
+nn=500;
+m = 20;
 Random.seed!(0)
 A=matrixdepot("wathen",nn,nn)
-#A = Diagonal([1, 2, 30, 3, 8, 9, 4]);
 n = size(A)[2];
 b = randn(n)
 
-## "True" values
+## "True" values for comparison
 vals, vecs = eigs(A)
 
 ## AM and Galerkin in same loop:
-Q = zeros(n,m+1);
+Q = zeros(n,m+1); # Initiate Q and H matrices
 H = zeros(m+1,m);
 Q[:,1] = b/norm(b);
 
-K = zeros(n,m);
+K = zeros(n,m); # Initiate Km
 K[:,1] = b;
 
-plot()
+plt = scatter(xlabel = "m", ylabel = "Real part of eigenval. approx.", xlims = [0, m], ylims = [0, 500]) # Set axes for plot
 
 for k=1:m
-    # Galerkin:
+    # Build Km for Galerkin method:
     if k >= 2
         Akb = A*K[:,k-1];
         K[:,k] = Akb/norm(Akb)
     end
-
-    # Arnoldi:
+    # Steps of Arnoldi Method:
     w=A*Q[:,k]; # Matrix-vector product with last element
-    # Orthogonalize w against columns of Q.
-    # Implement this function or replace call with code for orthogonalizatio
-    h,β,z = my_hw1_gs(Q[:,1:k],w);
-    #Put Gram-Schmidt coefficients into H
-    H[1:(k+1),k] = [h;β];
-    # normalize
-    Q[:,k+1] = z/β;
+    h,β,z = my_hw1_gs(Q[:,1:k],w); # Orthogonalize w against columns of Q
+    H[1:(k+1),k] = [h;β]; #Put Gram-Schmidt coefficients into H
+    Q[:,k+1] = z/β; # normalize
 
+    # Obtain eigenvalue approximations from the respective methods
     galerkin_vals, galerkin_vecs = eigen(K[:,1:k]'*A*K[:,1:k], K[:,1:k]'*K[:,1:k])
-    AM_vals, AM_vecs = eigen(Q[:,1:k+1]'*A*Q[:,1:k+1])
-    display(galerkin_vals)
-    display(AM_vals)
+    AM_vals, AM_vecs = eigen(Q[:,1:k]'*A*Q[:,1:k])
 
-    plot!(AM_vals, label = "Eigenvalue approx from AM", seriestype = :scatter, color = :black, marker = :circle, markersize = 4)
-    plot!(galerkin_vals, label = "Eigenvalue approx from (2)", seriestype = :scatter, color = :red, marker = :x, markersize = 4)
+    # Plot the results
+    if k == 1
+        scatter!(plt, k*ones(length(AM_vals)), AM_vals, color = :black, marker = :circle, label = "Eigenvalue approx from Arnoldi method", markersize = 2, legend = :topleft)
+        scatter!(plt, k*ones(length(galerkin_vals)), galerkin_vals, color = :red, marker = :x, label = "Eigenvalue approx from (2)", markersize = 2)
+        display(plt)
+    else
+        scatter!(plt, k*ones(length(AM_vals)), AM_vals, color = :black, marker = :circle, label = nothing, markersize = 2)
+        scatter!(plt, k*ones(length(galerkin_vals)), real.(galerkin_vals), color = :red, marker = :x, label = nothing, markersize = 2)
+        display(plt)
+    end
 end
-
-## Arnoldi Method
-Q, H = arnoldi(A,b,m)
-AM_vals, AM_vecs = eigen(Q'*A*Q)
-display(norm(Q'*Q - I))
--- HW1/ex6.jl
+++ HW1/ex6.jl
@@ -1,35 +1,47 @@
 include("arnoldi.jl")
-using MAT, LinearAlgebra, Arpack#, Plots
+using MAT, LinearAlgebra, Arpack, Plots
 
 B = matread("Bwedge.mat")["B"];
 B_eigvals = matread("Bwedge.mat")["B_eigvals"];
 
-## a) + b) Plot eigenvalues and mark those that will converge fastest with AM.
-# Indicate with circles and give estimations for convergence factors
-outside = [-47.0161 + 0.1659im, 0.9856 - 11.8979im, 1.3137 + 12.6637im];
-plot(B_eigvals, legend = :topleft, label = "Eigenvalues", seriestype = :scatter, color = :black, marker = :x, markersize = 2)
-plot!(outside, label = "Outside Eigenvalues", seriestype = :scatter, color = :red, marker = :circle, markersize = 4)
+## a) + b)
+outside = B_eigvals[[1,2,3]]; # find outer eigenvalues
+# Plots
+#plt = plot(B_eigvals, legend = :topleft, label = "Eigenvalues", seriestype = :scatter, color = :black, marker = :x, markersize = 2)
+plt = plot(xlims = [-50, 20], ylims = [-20, 20], B_eigvals, legend = :topleft, label = "Eigenvalues", seriestype = :scatter, color = :black, marker = :x, markersize = 2)
+plot!(plt, outside, label = "Outer eigenvalues", seriestype = :scatter, color = :red, marker = :circle, markersize = 4)
 
 ## c)
-
 n = size(B)[2];
+Random.seed!(0)
 b = randn(n);
+plt = scatter(xlims = [-48, 3], ylims = [-13, 13])
 
 for m = [2 4 8 10 20 30 40]
-    Q, H = arnoldi(B,b,m);
-    AM_vals, AM_vecs = eigen(Q'*B*Q);
-    p = plot(AM_vals, legend = :topleft, label = "Ritz values", seriestype = :scatter, color = :black, marker = :o, markersize = 3)
-    display(p)
-    sleep(0.5)
-    error_AM = AM_vals[[1 m m+1]] - transpose(outside)
-    display(norm(error_AM))
+    Q, H = arnoldi(B,b,m-1); # Generate Qm and Hm
+    AM_vals, AM_vecs = eigen(Q'*B*Q); # Calculate Ritz values
+    scatter!(plt, AM_vals, label = string("m = ", m), legend = :topleft, marker = :o, markersize = 3)
+    display(plt)
+    savefig(plt, string("plot_ex6c_m=", m, ".png"))
+    error_AM = AM_vals[[1 m m-1]] - transpose(outside)
+    display(string(error_AM[1], " & ",error_AM[2], " & ", error_AM[3]))
 end
 
 ## d) Shift and invert
-for m = [10 20 30]
-    σ = -9.8 + 1.5im;
+target_eig = B_eigvals[findmin(abs.(B_eigvals .- (-9.8 + 1.5im)))[2]] #the eigenvalue in question
+
+for σ = [-10, -7 + 2im, -9.8 + 1.5im]
     A = inv(B - σ * I);
-    Q, H = arnoldi(A,b,m);
-    AM_vals, AM_vecs = eigen(Q'*A*Q);
-    display(σ .+ 1 ./ AM_vals)
+    display(σ)
+    for m = [10 20 30]
+        display(m)
+        Q, H = arnoldi(A,b,m-1);
+        AM_vals, AM_vecs = eigen(Q'*A*Q);
+        eig_approx = σ .+ 1 ./ AM_vals
+        p = plot(eig_approx, legend = :topleft, label = "Eigenvalue approx", seriestype = :scatter, color = :black, marker = :o, markersize = 3, xlim = [-50,5], ylim = [-13,13])
+        display(p)
+        sleep(0.5)
+        closest = eig_approx[findmin(abs.(eig_approx .- target_eig))[2]]
+        display(abs(closest - target_eig))
+    end
 end
