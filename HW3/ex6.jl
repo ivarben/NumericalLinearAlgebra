7654757fb2ee7983ace8e28dbaff449d254a2ac5
ERATO <erato@Ivars-MBP.lan>
2020-12-09 12:27:15 +0100

    onsdag lunch

---
+++ HW3/ex6.jl
@@ -0,0 +1,23 @@
+using LinearAlgebra, Plots
+
+plt = scatter(xscale=:log, yscale=:log, xlabel="ε", ylabel="||f(A)-F||")
+for power = -10:-1
+
+    ε = 10.0^power;
+    A = [π 1; 0 π+ε];
+
+    ## find exp(A) using JCF definition according to example in 4.1.2
+    E = eigen(A)
+    FJ = E.vectors*Diagonal(exp.(E.values))*inv(E.vectors)
+
+    ## find exp(A) using the formula in b)
+    β = (exp(π+ε) - exp(π))/ε;
+    α = exp(π) - β*π;
+    V = [1 1; 0 ε];
+    V_inv = [1 -1/ε; 0 1/ε];
+    F = V*Diagonal(α .+ β*[π; π+ε])*V_inv;
+
+    error = norm(F-FJ);
+    scatter!(plt, [ε], [error], label=nothing, marker=:x, color = :red)
+end
+display(plt)
